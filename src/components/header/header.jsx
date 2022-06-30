e083d0ad0eb9d77d59f09366f7fb062b28c5a3d4
NarayanaRavada <lakshminarayanaravada777@gmail.com>
2022-06-30 23:13:35 +0530

    main, preview, auth added

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import { Link, Navigate } from "react-router-dom";
-import { auth } from "../../firebase/firebase.utils";
-import { connect } from "react-redux";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
-import "./header.styles.scss";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import './header.styles.scss';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
         <Logo className="logo" />
+        Mystic Store
       </Link>
       <div className="options">
         <Link className="option" to="/shop">
